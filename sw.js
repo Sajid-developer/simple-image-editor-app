c028053f12c2662378a8b78b8f78be262738787f
Er. Sajid Alam <msasajid01.pro@gmail.com>
2025-06-18 23:15:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
+// custom install button for PWA
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent default mini-infobar
+  e.preventDefault();
+  deferredPrompt = e;
+
+  // Show your custom install button
+  document.getElementById('installButton').style.display = 'block';
+});
+const installButton = document.getElementById('installButton');
+
+installButton.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt(); // Show native prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    deferredPrompt = null;
+    installButton.style.display = 'none';
+  }
+});
+
 // On install - caching the application shell
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
+    caches.open('sw-cache').then(function (cache) {
       // cache any static files that make up the application shell
       return cache.add('index.html');
     })
@@ -9,10 +32,10 @@ self.addEventListener('install', function(event) {
 });
 
 // On network request
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     // Try the cache
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       //If response found return it, else fetch again
       return response || fetch(event.request);
     })
